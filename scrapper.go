21827e92df832ab79bd5c62437ea9f5ae5dfa3be
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2024-03-17 00:15:06 -0300

    feat: create basic scrapper

--- scrapper.go
+++ scrapper.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"github.com/gocolly/colly"
+)
+
+type musicData struct {
+	Title  string `json:"title"`
+	Artist string `json:"artist"`
+	Rank   int    `json:"rank"`
+}
+
+func main() {
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.billboard.com"),
+	)
+
+	var music []musicData
+	rank := 1
+
+	c.OnHTML("div[class=o-chart-results-list-row-container] li.lrv-u-flex-grow-1", func(e *colly.HTMLElement) {
+		title := e.ChildText("h3[id=title-of-a-story]")
+		artist := e.ChildText("span.a-font-primary-s")
+
+		if title != "" && artist != "" {
+			music = append(music, musicData{
+				Title:  title,
+				Artist: artist,
+				Rank:   rank,
+			})
+			rank++
+		}
+	})
+
+	// Start scraping
+	c.Visit("https://www.billboard.com/charts/hot-100/1991-07-13/")
+
+	// Print data
+	for _, m := range music {
+		println(m.Rank, m.Title, m.Artist)
+	}
+}
